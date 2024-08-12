cb002448d2b480fcf9b7bc79566ba166a94c4757
Nick DeFelice <ndefelice28@gmail.com>
2024-08-12 14:43:52 -0400

    League rules page

---
+++ src/app/leagues/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>This is one of the many leagues pages</h1>
+      </div>
+    );
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the default home page.</p>
+    </div>
+  );
+}

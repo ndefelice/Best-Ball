85570a0343f89de9c256f9771314cde78afc3c5e
Nick DeFelice <ndefelice28@gmail.com>
2024-09-05 17:09:30 -0400

    No need for backend, removed

--- server/models/leagues.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const leaguesSchema = new mongoose.Schema({
-    league_id: Number,
-});
-
-const Leagues = mongoose.model('Leagues', leaguesSchema);
-
-module.exports = Leagues;

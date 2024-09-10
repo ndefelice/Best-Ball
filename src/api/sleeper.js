de40356296e201cf78ecf5f427c7fe9d7a759875
Dylan Vasapollo <dmvasapo@buffalo.edu>
2024-09-10 10:29:59 -0400

    Created fetchUserAvatar func, still needs testing

--- src/api/sleeper.js
+++ src/api/sleeper.js
@@ -83,3 +83,34 @@ async function fetchDraftResults(draft_id) {
 }
 
 export { fetchLeagueStandings };
+
+// Pulls user profile photo and displays it next to their team name
+// api calls to user avatar are either https://sleepercdn.com/avatars/<avatar_id> (full size image)
+// or https://sleepercdn.com/avatars/thumbs/<avatar_id> (thumbnail)
+// avatar_id for a specific user can be found in the json response for user object
+
+async function fetchUserAvatar(league_id){
+  try {
+    const response = await fetch(
+      `https://api.sleeper.app/v1/league/${league_id}/users`,
+    );
+    const users = await response.json();
+    const user_info = {};
+
+    users.forEach((user) => {
+      const userId = user.user_id;
+      const displayName = user.display_name;
+      const userAvatar = user.avatar;
+
+      const userAvatarUrl = userAvatar ? 'https://sleepercdn.com/avatars/thumbs/${userAvatarId}' : null; // null if user has no avatar
+
+      user_info[userId] = {
+        displayName: displayName,
+        avatarUrl: userAvatarUrl,
+      };
+    });
+    return user_info;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}

9912122b2c349538d45588f2456d7b0c68596f8b
Nick DeFelice <ndefelice28@gmail.com>
2024-11-27 15:30:35 -0500

    Update API route

--- src/api/playoffs.js
+++ src/api/playoffs.js
@@ -1,7 +1,7 @@
 const API_URL = 'https://best-ball-api-docker.onrender.com'; // Update with your server URL if deployed
 
 export const fetchAllPlayoffUsers = async () => {
-    const response = await fetch(`${API_URL}/playoffs`);
+    const response = await fetch(`${API_URL}/playoffs?week=1`);
     if (!response.ok) {
         throw new Error('Failed to fetch playoff users');
     }
